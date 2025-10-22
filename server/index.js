4311938c0a917e95b10d7a44b055530b7b4d5185
JoaoNasa <joaogabrielnasario@gmail.com>
2025-10-22 20:12:18 -0300

    Ultilizando a pasta env para configurar a URL/Key da SUPABASE

--- server/index.js
+++ server/index.js
@@ -18,3 +18,18 @@ const app = express();
 
 //configurando a representatividade, indicamos para o servidor que ele vai receber e enviar um arquivo json
 app.use(express.json())
+
+//conexao com o banco de dados no SupaBase(CLOUD)
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+
+app.post("/pessoa",(req,res) => {
+
+    res.status(200).json( { message: "Deu certo o endpoint!!" } )
+
+})
+
+app.listen(3000, () => {
+
+    console.log("O servidor subiu para a porta 3000")
+
+})
