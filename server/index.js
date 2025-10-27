109c85f090af6ef72e53a6e88ae9ae4efb7ee26c
JoaoNasa <joaogabrielnasario@gmail.com>
2025-10-27 19:42:09 -0300

    atualizando a mensagem de erro e adicionando o insert para o nome/idade/curso do aluno

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // configurar os imports das bibliotecas
 
+
 import express from "express";
 
 import dotenv from "dotenv";
@@ -22,9 +23,17 @@ app.use(express.json())
 //conexao com o banco de dados no SupaBase(CLOUD)
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
-app.post("/pessoa",(req,res) => {
+app.post("/pessoa", async (req,res) => {
 
-    res.status(200).json( { message: "Deu certo o endpoint!!" } )
+    const {nome, idade, curso} = req.body; //novo
+    
+    const{data , error} =  await supabase //novo
+    .from("pessoa") //novo
+    .insert([{nome, idade, curso}]);//novo
+
+    if (error) return res.status(400).json({message: "Erro ao inserir a pessoa"}) //novo
+
+    res.status(200).json( { message: "Deu certo o endpoint!!" } ) //novo
 
 })
 
@@ -33,3 +42,9 @@ app.listen(3000, () => {
     console.log("O servidor subiu para a porta 3000")
 
 })
+
+
+
+//npm install express dotenv @supabase/supabase-js
+
+//localhost:3000/pessoa
